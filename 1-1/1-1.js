3f0c13b98fc0fe6b6793670b15cbfd4828804f4d
a001560 <austincqdo@berkeley.edu>
2019-07-01 17:34:22 +0900

    added 1-1 1-2 1-3

---
+++ 1-1/1-1.js
@@ -0,0 +1,32 @@
+const fileManage = {
+    'f001': {'Folder Name': 'Information System Department',
+        'View Auth': 'Information System Department',
+        'Edit Auth': 'Information System Department',
+        'Delete Auth': 'Information System Department'},
+    'f002': {'Folder Name': 'Corporate Planning Department',
+        'View Auth': 'Corporate Planning Department',
+        'Edit Auth': 'Corporate Planning Department',
+        'Delete Auth': 'Corporate Planning Department'},
+    'f003': {'Folder Name': 'Sales Department',
+        'View Auth': 'Everyone',
+        'Edit Auth': 'Sales Department',
+        'Delete Auth': 'Sales Department'},
+    'f004': {'Folder Name': 'Development Division',
+        'View Auth': 'Development Division',
+        'Edit Auth': 'Development Division',
+        'Delete Auth': 'Development Division'},
+    'f005': {'Folder Name': 'General Affairs Department',
+        'View Auth': 'Everyone',
+        'Edit Auth': 'Everyone',
+        'Delete Auth': 'General Affairs Department'},
+    'f006': {'Folder Name': 'Accounting Department',
+        'View Auth': 'Accounting Department',
+        'Edit Auth': 'Accounting Department',
+        'Delete Auth': 'Accounting Department'},
+    'f007': {'Folder Name': 'Planning Department',
+        'View Auth': 'Everyone',
+        'Edit Auth': 'Planning Department',
+        'Delete Auth': 'Planning Department'
+    }
+};
+console.log(fileManage);
