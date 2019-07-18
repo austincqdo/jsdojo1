4c930242a8c7120f0cadfd2f812ad049f5c16639
a001560 <austincqdo@berkeley.edu>
2019-07-18 16:43:01 +0900

    made fixes to map.js

--- map.js
+++ map.js
@@ -1,6 +1,5 @@
 (function() {
 	'use strict';
-	let mymap;
 	kintone.events.on('app.record.detail.show', function(event) {
 		const record = event.record;
 		const address = record.Address.value;
@@ -8,10 +7,12 @@
 		const cybozuAddress = [35.68704, 139.7861];
 		const name = record.Place.value;
 
-		headerSpace.style.height = '300px';
-		headerSpace.setAttribute('id', 'nihonbashi');
+		const mapSpace = document.createElement('div');
+		mapSpace.style.height = '300px';
+		mapSpace.setAttribute('id', 'nihonbashi');
+		headerSpace.appendChild(mapSpace);
 
-		mymap = L.map('nihonbashi').setView(cybozuAddress, 16);
+		const mymap = L.map('nihonbashi').setView(cybozuAddress, 16);
 		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
 	  		maxZoom: 18
 		}).addTo(mymap);
@@ -23,24 +24,23 @@
 		const reference = L.marker(cybozuAddress).addTo(mymap);
 		reference.bindPopup('Nihombashi Tower', {autoClose: false}).openPopup();
 
-		forwardGeocode(name, address);
+		forwardGeocode(name, address, mymap);
 	})
 
-	function forwardGeocode(name, address) {
+	function forwardGeocode(name, address, map) {
 		const encodedAddress = encodeURI(address);
 		const settings = {
 			'crossDomain': true,
 			'url': 'https://us1.locationiq.com/v1/search.php?key=pk.7fa009bf60cb8aef22d9f72909124750&q=' + encodedAddress + '&format=json',
 			'method': 'GET'
 		}
+		function setMarker(name, address, map) {
+			const restaurant = L.marker(address).addTo(map);
+			restaurant.bindPopup(name, {autoClose: false}).openPopup();
+		}
 		$.ajax(settings).done(function(response) {
 			const address = [parseFloat(response[0].lat), parseFloat(response[0].lon)];
-			setMarker(name, address);
+			setMarker(name, address, map);
 		});
-	}
-
-	function setMarker(name, address) {
-		const restaurant = L.marker(address).addTo(mymap);
-		restaurant.bindPopup(name, {autoClose: false}).openPopup();
 	}
 })();
