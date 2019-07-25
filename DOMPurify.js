49d4ec1bc34d9882fd3492808d387d3558793dd5
a001560 <austincqdo@berkeley.edu>
2019-07-25 15:55:21 +0900

    reformatted, added if check

--- DOMPurify.js
+++ DOMPurify.js
@@ -1,11 +1,18 @@
 (function() {
-	events = ['app.record.create.submit', 'app.record.edit.submit', 'app.record.index.edit.submit'];
+	const events = ['app.record.create.submit', 
+					'app.record.edit.submit', 
+					'app.record.index.edit.submit'];
+	const fields_to_clean = ['product', 'quantity', 'price'];
 	kintone.events.on(events, function(event) {
 		const record = event.record;
 		for (let field in record) {
-			const input = record[field].value;
-			const clean_input = DOMPurify.sanitize(input);
-			record[field].value = clean_input;
+			if (record.hasOwnProperty(field)) {
+				if (field in fields_to_clean) {
+					const input = record[field].value;
+					const clean_input = DOMPurify.sanitize(input);
+					record[field].value = clean_input;
+				}
+			}
 		}
 		return event;
 	});
