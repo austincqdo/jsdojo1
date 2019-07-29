484261635e5469c487d3646b0e985f5bb98d5c90
a001560 <austincqdo@berkeley.edu>
2019-07-29 10:25:12 +0900

    removed extraneous files

--- 3-7/3-7.js
+++
@@ -1,8 +0,0 @@
-// Battle
-var hp = 100;
-while (hp > 0) {
-	var atk = Math.floor(11 * Math.random());
-	alert("You dealt " + atk + " damage!");
-	hp -= atk;
-}
-alert("You have slain the kraken!");
