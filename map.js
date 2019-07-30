3a65b8e864793ce58bf3f1a9ee3263130f92e725
a001560 <austincqdo@berkeley.edu>
2019-07-30 14:21:00 +0900

    changed ajax call to kintone.proxy

--- map.js
+++ map.js
@@ -4,42 +4,41 @@
 		const record = event.record;
 		const address = record.Address.value;
 		const headerSpace = kintone.app.record.getHeaderMenuSpaceElement();
-		const cybozuAddress = [35.68704, 139.7861];
+		const kintoneAddress = [37.789808, -122.401767];
 		const name = record.Place.value;
 
 		const mapSpace = document.createElement('div');
 		mapSpace.style.height = '300px';
-		mapSpace.setAttribute('id', 'nihonbashi');
+		mapSpace.setAttribute('id', 'kintone');
 		headerSpace.appendChild(mapSpace);
 
-		const mymap = L.map('nihonbashi').setView(cybozuAddress, 16);
+		const kintoneMap = L.map('kintone').setView(kintoneAddress, 16);
 		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
 	  		maxZoom: 18
-		}).addTo(mymap);
+		}).addTo(kintoneMap);
 		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png', {
 		 	maxZoom: 18,
 		 	zIndex: 10
-		}).addTo(mymap);
+		}).addTo(kintoneMap);
 		
-		const reference = L.marker(cybozuAddress).addTo(mymap);
-		reference.bindPopup('Nihombashi Tower', {autoClose: false}).openPopup();
+		const reference = L.marker(kintoneAddress).addTo(kintoneMap);
+		reference.bindPopup('Kintone USA', {autoClose: false}).openPopup();
 
-		forwardGeocode(name, address, mymap);
+		forwardGeocode(name, address, kintoneMap);
 	})
 
 	function forwardGeocode(name, address, map) {
 		const encodedAddress = encodeURI(address);
-		const settings = {
-			'crossDomain': true,
-			'url': 'https://us1.locationiq.com/v1/search.php?key=pk.7fa009bf60cb8aef22d9f72909124750&q=' + encodedAddress + '&format=json',
-			'method': 'GET'
-		}
+		const URL = 'https://us1.locationiq.com/v1/search.php?key=pk.7fa009bf60cb8aef22d9f72909124750&q=' + encodedAddress + '&format=json';
+		const method = 'GET';
 		function setMarker(name, address, map) {
 			const restaurant = L.marker(address).addTo(map);
 			restaurant.bindPopup(name, {autoClose: false}).openPopup();
 		}
-		$.ajax(settings).done(function(response) {
-			const address = [parseFloat(response[0].lat), parseFloat(response[0].lon)];
+		kintone.proxy(URL, method, {}, {}, function(response) {
+			const response_array = JSON.parse(response);
+			const address = [parseFloat(response_array[0].lat), parseFloat(response_array[0].lon)];
+			console.log(address);
 			setMarker(name, address, map);
 		});
 	}
