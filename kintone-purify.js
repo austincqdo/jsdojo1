4c2bc36c12c55cb612fad9130457bf8ecef6c112
a001560 <austincqdo@berkeley.edu>
2019-07-31 11:27:59 +0900

    changed filename to kintone-purify.js

---
+++ kintone-purify.js
@@ -0,0 +1,19 @@
+(function() {
+	const events = ['app.record.create.submit', 
+					'app.record.edit.submit', 
+					'app.record.index.edit.submit'];
+	const fields_to_clean = ['product'];
+	kintone.events.on(events, function(event) {
+		const record = event.record;
+		for (let field in record) {
+			if (record.hasOwnProperty(field)) {
+				if (fields_to_clean.includes(field)) {
+					const input = record[field].value;
+					const clean_input = DOMPurify.sanitize(input);
+					record[field].value = clean_input;
+				}
+			}
+		}
+		return event;
+	});
+})();
