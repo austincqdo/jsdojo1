d7751e17cb85e078c2cad3d997fba24b434d7646
a001560 <austincqdo@berkeley.edu>
2019-08-01 17:58:56 +0900

    changed variable names to camelcase

--- kintone-purify.js
+++ kintone-purify.js
@@ -2,14 +2,14 @@
     const events = ['app.record.create.submit',
                     'app.record.edit.submit',
                     'app.record.index.edit.submit'];
-    const fields_to_clean = ['product'];
+    const fieldsToClean = ['product'];
     kintone.events.on(events, function(event) {
         const record = event.record;
         Object.keys(record).forEach(function(field) {
-            if (fields_to_clean.includes(field)) {
+            if (fieldsToClean.includes(field)) {
                 const input = record[field].value;
-                const clean_input = DOMPurify.sanitize(input);
-                record[field].value = clean_input;
+                const cleanInput = DOMPurify.sanitize(input);
+                record[field].value = cleanInput;
             }
         });
         return event;
