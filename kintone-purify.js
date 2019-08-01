659625faec309ad3cab84ce31b9df47673ec3349
a001560 <austincqdo@berkeley.edu>
2019-08-01 17:50:47 +0900

    ran eslint

--- kintone-purify.js
+++ kintone-purify.js
@@ -1,19 +1,17 @@
 (function() {
-	const events = ['app.record.create.submit', 
-					'app.record.edit.submit', 
-					'app.record.index.edit.submit'];
-	const fields_to_clean = ['product'];
-	kintone.events.on(events, function(event) {
-		const record = event.record;
-		for (let field in record) {
-			if (record.hasOwnProperty(field)) {
-				if (fields_to_clean.includes(field)) {
-					const input = record[field].value;
-					const clean_input = DOMPurify.sanitize(input);
-					record[field].value = clean_input;
-				}
-			}
-		}
-		return event;
-	});
+    const events = ['app.record.create.submit',
+                    'app.record.edit.submit',
+                    'app.record.index.edit.submit'];
+    const fields_to_clean = ['product'];
+    kintone.events.on(events, function(event) {
+        const record = event.record;
+        Object.keys(record).forEach(function(field) {
+            if (fields_to_clean.includes(field)) {
+                const input = record[field].value;
+                const clean_input = DOMPurify.sanitize(input);
+                record[field].value = clean_input;
+            }
+        });
+        return event;
+    });
 })();
