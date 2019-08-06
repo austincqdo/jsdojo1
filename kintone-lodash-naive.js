bc739d20ef25a1bd13572e4a445b7c606106ef97
a001560 <austincqdo@berkeley.edu>
2019-08-06 16:47:39 +0900

    stored field codes in variables

--- kintone-lodash-naive.js
+++ kintone-lodash-naive.js
@@ -1,7 +1,15 @@
 (function() {
 	'use strict';
-	kintone.events.on(['app.record.detail.show', 'app.record.edit.change.sort'], function(event) {
-		const array_of_table_rows = event.record.table.value;
+	const checkbox_field = 'sort';
+	const events = [
+		'app.record.detail.show',
+		'app.record.edit.change.' + checkbox_field
+		];
+	kintone.events.on(events, function(event) {
+		const space_id = 'space';
+		const table_field = 'table';
+
+		const array_of_table_rows = event.record[table_field].value;
 		const sort_input = event.record.sort.value;
 		const employee_roles = {};
 		if (sort_input) {
@@ -24,7 +32,7 @@
 				'ü': 'u'
 				// and so on...
 			};
-			const blank_space = kintone.app.record.getSpaceElement('space');
+			const blank_space = kintone.app.record.getSpaceElement(space_id);
 			const list_area = document.createElement('div');
 			const ul = document.createElement('ul');
 			while (blank_space.firstChild) {
