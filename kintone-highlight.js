8377cc400c6f4d1de2c9dd6e788894ec51077579
a001560 <austincqdo@berkeley.edu>
2019-08-06 13:32:17 +0900

    removed blank space code

--- kintone-highlight.js
+++ kintone-highlight.js
@@ -20,18 +20,6 @@
 		text_pre.appendChild(text_code);
 		text_area.appendChild(text_pre);
 
-		// Blank space
-		const blank_space = kintone.app.record.getSpaceElement('blank');
-		const space_pre = document.createElement('pre');
-		const space_code = document.createElement('code');
-		space_pre.setAttribute('style', 'margin-left: -200px');
-		space_code.innerText = `
-			<script type="application/javascript">
-  			    function $init() {return true;}
-			</script>
-			`;
-		space_pre.appendChild(space_code);
-		blank_space.appendChild(space_pre);
 		// The only way we can delay instantiation for highlight.js 
 		// is to process each block individually using querySelectorAll().
 		document.querySelectorAll('pre code').forEach(function(block) {
