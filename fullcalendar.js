1a1708686f75a637bc9ee34837d25c81c1bd8b4d
a001560 <austincqdo@berkeley.edu>
2019-08-07 11:20:13 +0900

    event clickable, error on erroneous submit, used kintone.api, moved parseInt, changed viewType to viewName, included app.record.index.edit.show

--- fullcalendar.js
+++ fullcalendar.js
@@ -10,7 +10,8 @@
                     'app.record.create.show',
                     'app.record.create.change.' + RADIOBUTTON,
                     'app.record.edit.show',
-                    'app.record.edit.change.' + RADIOBUTTON];
+                    'app.record.edit.change.' + RADIOBUTTON,
+                    'app.record.index.edit.show'];
 
     // Only show option to input start and end times if 'All day' not selected.
     kintone.events.on(events, function(event) {
@@ -27,41 +28,12 @@
         }
     });
 
-    // Instantiate FullCalendar instance
-    kintone.events.on('app.record.index.show', function(event) {
-        if (event.viewType === 'custom') {
-            const calendarEl = document.getElementById('calendar');
-            const calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: ['dayGrid'],
-                displayEventEnd: true
-            });
-            const records = event.records;
-            for (const i in records) {
-                if (records[i][RADIOBUTTON].value === RADIOVALUE) {
-                    const dateSplit = (records[i][DATEFIELD].value).split('-');
-                    const year = parseInt(dateSplit[0]);
-                    const month = parseInt(dateSplit[1]) - 1;
-                    const day = parseInt(dateSplit[2]);
-                    const sHour = parseTime(records[i][STIME].value)[0];
-                    const sMinute = parseTime(records[i][STIME].value)[1];
-                    const eHour = parseTime(records[i][ETIME].value)[0];
-                    const eMinute = parseTime(records[i][ETIME].value)[1];
-
-                    calendar.addEvent({
-                        title: records[i][TEXTFIELD].value,
-                        start: new Date(year, month, day, sHour, sMinute),
-                        end: new Date(year, month, day, eHour, eMinute)
-                    });
-                } else {
-                    calendar.addEvent({
-                        title: records[i][TEXTFIELD].value,
-                        start: records[i][DATEFIELD].value,
-                        allDay: true
-                    });
-                }
-            }
-            calendar.render();
+    // Prevent user from submitting event if start_time is after end_time
+    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function(event) {
+        if (event.record[STIME].value > event.record[ETIME].value) {
+            event.record[RADIOBUTTON].error = 'End time must occur after Start time.';
         }
+        return event;
     });
 
     function parseTime(time) {
@@ -70,4 +42,51 @@
         const minute = parseInt(timeSplit[1]);
         return [hour, minute];
     }
+
+    // Instantiate FullCalendar instance
+    kintone.events.on('app.record.index.show', function(event) {
+        const appId = kintone.app.getId();
+        if (event.viewName === 'FullCalendar') {
+            const calendarEl = document.getElementById('calendar');
+            const calendar = new FullCalendar.Calendar(calendarEl, {
+                plugins: ['dayGrid'],
+                displayEventEnd: true,
+                eventClick: function(info) {
+                    window.location.href = info.url;
+                }
+            });
+            kintone.api(kintone.api.url('/k/v1/records', true), 'GET', {'app': appId}, function(response) {
+                const records = response.records;
+                records.forEach(function(record) {
+                    const appURL = '/k/' + appId + '/show#record=' + record.$id.value;
+                    if (record[RADIOBUTTON].value === RADIOVALUE) {
+                        const dateSplit = (record[DATEFIELD].value).split('-');
+                        const year = parseInt(dateSplit[0]);
+                        const month = parseInt(dateSplit[1]) - 1;
+                        const day = parseInt(dateSplit[2]);
+                        const sHour = parseTime(record[STIME].value)[0];
+                        const sMinute = parseTime(record[STIME].value)[1];
+                        const eHour = parseTime(record[ETIME].value)[0];
+                        const eMinute = parseTime(record[ETIME].value)[1];
+                        calendar.addEvent({
+                            title: record[TEXTFIELD].value,
+                            start: new Date(year, month, day, sHour, sMinute),
+                            end: new Date(year, month, day, eHour, eMinute),
+                            url: appURL
+                        });
+                    } else {
+                        calendar.addEvent({
+                            title: record[TEXTFIELD].value,
+                            start: record[DATEFIELD].value,
+                            allDay: true,
+                            url: appURL
+                        });
+                    }
+                });
+                calendar.render();
+            }, function(error) {
+                console.log(error);
+            });
+        }
+    });
 })();
